9980cf36105d8d083980e98cb138bd6ff195f8dd
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 07:47:48 -0300

    Upgrade Factorization to existential (nontrivial form)

--- FMT/Types/Factorization.lean
+++ FMT/Types/Factorization.lean
@@ -1,5 +1,6 @@
 namespace FMT.Types
 
-def FactorsThrough {G : Type} (f : G → Nat) (τ : G → Type) : Prop := True
+def FactorsThrough {G : Type} (f : G → Nat) (τ : G → Type) : Prop :=
+  ∃ g : τ → Nat, ∀ x, f x = g (τ x)
 
 end FMT.Types
