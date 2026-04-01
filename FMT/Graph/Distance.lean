2cd190d544a8592eac4ee9b4a29974304f2fcc86
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 08:08:23 -0300

    Fix Distance: remove dependent Walk parameters (type-correct)

--- FMT/Graph/Distance.lean
+++ FMT/Graph/Distance.lean
@@ -2,9 +2,9 @@ import FMT.Graph.Basic
 
 namespace FMT.Graph
 
-def Walk (G : Graph) (u v : G.V) := List G.V
+def Walk (G : Graph) : Type := List G.V
 
-def walkLength {G : Graph} (w : Walk G u v) : Nat := w.length
+def walkLength {G : Graph} (w : Walk G) : Nat := w.length
 
 def dist (G : Graph) (u v : G.V) : Nat := 0
 
