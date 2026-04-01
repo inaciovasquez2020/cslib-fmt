0330ce50f5601974b49b2d1bef9e7ad07e1a2eb4
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 07:54:48 -0300

    Add LocalType stub (compile-safe layer)

--- FMT/Types/LocalType.lean
+++ FMT/Types/LocalType.lean
@@ -1,15 +1,5 @@
-import FMT.Graph.Basic
-
 namespace FMT.Types
 
-structure RootedBall (G : Type) where
-  center : G
-  radius : Nat
-
-structure LocalType where
-  code : Nat
-
-def encode {G : Type} (b : RootedBall G) : LocalType :=
-  ⟨b.radius⟩
+def LocalType (G : Type) : Type := Unit
 
 end FMT.Types
