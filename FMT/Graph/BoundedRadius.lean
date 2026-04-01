2f092ce54119061aac1de8e4f3cb3cd24d95dca7
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 08:52:55 -0300

    Simplify Ball: remove redundant conjunction in subtype predicate

--- FMT/Graph/BoundedRadius.lean
+++ FMT/Graph/BoundedRadius.lean
@@ -4,6 +4,6 @@ import FMT.Graph.Distance
 namespace FMT.Graph
 
 def Ball (G : Graph) (r : Nat) (v : G.V) :=
-  { u : G.V // dist G v u ≤ r ∧ True }
+  { u : G.V // dist G v u ≤ r }
 
 end FMT.Graph
