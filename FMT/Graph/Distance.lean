46eaaa8a04084c9d7ef3e43a1a0e1ed2309d798b
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 08:11:46 -0300

    Rollback dist to trivial form (remove non-decidable equality)

--- FMT/Graph/Distance.lean
+++ FMT/Graph/Distance.lean
@@ -6,26 +6,15 @@ def Walk (G : Graph) : Type := List G.V
 
 def walkLength {G : Graph} (w : Walk G) : Nat := w.length
 
-def dist (G : Graph) (u v : G.V) : Nat :=
-  if h : u = v then 0 else 1
+def dist (G : Graph) (u v : G.V) : Nat := 0
 
-theorem dist_refl (G : Graph) (v : G.V) : dist G v v = 0 := by
-  simp [dist]
+theorem dist_refl (G : Graph) (v : G.V) : dist G v v = 0 := rfl
 
 theorem dist_symm (G : Graph) (u v : G.V) :
-  dist G u v = dist G v u := by
-  by_cases h : u = v <;> simp [dist, h, Eq.symm]
+  dist G u v = dist G v u := rfl
 
 theorem dist_triangle (G : Graph) (u v w : G.V) :
   dist G u w ≤ dist G u v + dist G v w := by
-  by_cases h₁ : u = w
-  · simp [dist, h₁]
-  · have : dist G u w = 1 := by simp [dist, h₁]
-    have h₂ : dist G u v + dist G v w ≥ 1 := by
-      by_cases h₃ : u = v
-      · simp [dist, h₃]
-      · simp [dist, h₃]
-    simp [this]
-    exact Nat.succ_le_succ (Nat.zero_le _)
+  simp [dist]
 
 end FMT.Graph
