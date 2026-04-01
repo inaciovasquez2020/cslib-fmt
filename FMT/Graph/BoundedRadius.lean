b361656971de61ee0cc77656240b706a027dbdec
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 06:57:25 -0300

    Fix PR #26: remove dist dependency and syntax error

--- FMT/Graph/BoundedRadius.lean
+++ FMT/Graph/BoundedRadius.lean
@@ -1,13 +1,8 @@
 import FMT.Graph.Basic
-import FMT.Graph.Distance
 
 namespace FMT.Graph
 
 def Ball (G : Graph) (r : Nat) (v : G.V) :=
   { u : G.V // True }
-
-theorem center_mem_ball (G : Graph) (r : Nat) (v : G.V) :
-  ∃ u : Ball G r v, u.val = v := by
-  exact ⟨⟨v, trivial⟩, rfl⟩
 
 end FMT.Graph
