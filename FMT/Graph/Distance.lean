4067b7b8923e1a419c6950f7f97240ffb42de577
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 09:08:42 -0300

    Introduce DecidableEq for vertices; enable nontrivial 0/1 metric

--- FMT/Graph/Distance.lean
+++ FMT/Graph/Distance.lean
@@ -2,10 +2,13 @@ import FMT.Graph.Basic
 
 namespace FMT.Graph
 
+variable [DecidableEq G.V]
+
 def dist (G : Graph) (u v : G.V) : Nat :=
-  0
+  if u = v then 0 else 1
 
 theorem dist_refl (G : Graph) (v : G.V) :
-  dist G v v = 0 := rfl
+  dist G v v = 0 := by
+  simp [dist]
 
 end FMT.Graph
