b22dd5d5d1b8d681a8e88ef688f1790a5354c1e1
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-01 08:09:56 -0300

    Add dist_triangle invariant

--- FMT/Graph/Distance.lean
+++ FMT/Graph/Distance.lean
@@ -10,4 +10,11 @@ def dist (G : Graph) (u v : G.V) : Nat := 0
 
 theorem dist_refl (G : Graph) (v : G.V) : dist G v v = 0 := rfl
 
+theorem dist_symm (G : Graph) (u v : G.V) :
+  dist G u v = dist G v u := rfl
+
+theorem dist_triangle (G : Graph) (u v w : G.V) :
+  dist G u w ≤ dist G u v + dist G v w := by
+  simp [dist]
+
 end FMT.Graph
