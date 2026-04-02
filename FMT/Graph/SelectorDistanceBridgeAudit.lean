59bd6b79446bb17b9d8f5c222a778c3d6026b7bf
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 07:20:09 -0300

    Audit selector-distance bridge from reachable completeness layer

---
+++ FMT/Graph/SelectorDistanceBridgeAudit.lean
@@ -0,0 +1,13 @@
+import FMT.Graph.ShortestPathSelectorReachableBridge
+import FMT.Graph.DistancePath
+import FMT.Graph.Distance
+
+namespace FMT.Graph
+
+#print shortest_path_selector_complete_of_reachable
+#print path_of_dist?_some
+#print dist?_le_of_path
+#print dist
+#print dist?
+
+end FMT.Graph
