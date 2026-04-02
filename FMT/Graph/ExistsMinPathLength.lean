fa2a19192e9ed9c7766458cbfcb6dbee8c528870
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 06:51:23 -0300

    Restore exists-min-path axiom and define selector from it

--- FMT/Graph/ExistsMinPathLength.lean
+++ FMT/Graph/ExistsMinPathLength.lean
@@ -3,10 +3,9 @@ import FMT.Graph.PathLength
 namespace FMT.Graph
 
 axiom exists_min_pathLength
-  (G : Graph) (u v : G.V) :
-  (∃ n : Nat, Nonempty (PathLength G u v n)) →
-  ∃ n : Nat,
-    Nonempty (PathLength G u v n) ∧
-    ∀ m : Nat, m < n → ¬ Nonempty (PathLength G u v m)
+  (G : Graph) (u v : G.V)
+  (h : ∃ n : Nat, Nonempty (PathLength G u v n)) :
+  ∃ k : Nat, Nonempty (PathLength G u v k) ∧
+    (∀ m : Nat, m < k → ¬ Nonempty (PathLength G u v m))
 
 end FMT.Graph
