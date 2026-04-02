fb24c139452480454a814348b99728dd20164ad8
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 07:13:21 -0300

    Audit concat-selector dependency chain from stable frontier

---
+++ FMT/Graph/ConcatSelectorDependencyAudit.lean
@@ -0,0 +1,14 @@
+import FMT.Graph.PathLengthConcat
+import FMT.Graph.DistanceTriangle
+import FMT.Graph.ShortestPathSelector
+import FMT.Graph.ExistsMinPathLength
+
+namespace FMT.Graph
+
+#print pathLength_concat
+#print distLE_triangle
+#print shortest_path_selector
+#print shortest_path_selector_complete
+#print exists_min_pathLength
+
+end FMT.Graph
