f9610219420466e09a15adf01d495be2e0901a03
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 06:56:51 -0300

    Audit selector frontier as next closure step

---
+++ FMT/Graph/ShortestPathSelectorAudit.lean
@@ -0,0 +1,11 @@
+import FMT.Graph.ShortestPathSelector
+import FMT.Graph.DistancePath
+
+namespace FMT.Graph
+
+#print shortest_path_selector
+#print shortest_path_selector_complete
+#print path_of_dist?_some
+#print dist?_le_of_path
+
+end FMT.Graph
