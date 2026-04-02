ceb79fb4d5986784ceb702ed120448dde6347583
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 01:43:12 -0300

    Replace IsEmpty with not-Nonempty in DistancePathBound

--- FMT/Graph/DistancePathBound.lean
+++ FMT/Graph/DistancePathBound.lean
@@ -2,8 +2,6 @@ import FMT.Graph.DistancePath
 
 namespace FMT.Graph
 
-universe u
-
 variable {G : Graph}
 
 theorem path_existence_to_distance_upper_bound
@@ -19,18 +17,17 @@ theorem shortest_path_exists_of_finite_distance
 theorem shortest_path_minimality
     {u v : G.V} {n : Nat} (h : dist? G u v = some n) :
     Nonempty (PathLength G u v n) ∧
-      ∀ m : Nat, m < n → IsEmpty (PathLength G u v m) := by
+      ∀ m : Nat, m < n → ¬ Nonempty (PathLength G u v m) := by
   constructor
   · exact path_of_dist?_some (G := G) h
-  · intro m hm
-    refine ⟨?_⟩
-    intro hmP
-    rcases dist?_le_of_path (G := G) (u := u) (v := v) (n := m) ⟨hmP⟩ with ⟨d, hd, hdm⟩
-    have : d = n := by
-      rcases (dist?_some_iff (G := G) (u := u) (v := v) (n := d)).1 hd with ⟨hexd, hfindd⟩
-      rcases (dist?_some_iff (G := G) (u := u) (v := v) (n := n)).1 h with ⟨hexn, hfindn⟩
-      rw [← hfindd, ← hfindn]
-    have hnm : n ≤ m := by simpa [this] using hdm
+  · intro m hm hmP
+    rcases dist?_le_of_path (G := G) (u := u) (v := v) (n := m) hmP with ⟨d, hd, hdm⟩
+    have hdn : d = n := by
+      rw [hd] at h
+      cases h
+      rfl
+    have hnm : n ≤ m := by
+      simpa [hdn] using hdm
     exact Nat.not_lt_of_ge hnm hm
 
 end FMT.Graph
