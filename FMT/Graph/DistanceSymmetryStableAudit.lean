a7010c0f9dc955a9965588e978c72884bb5d5386
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 07:12:23 -0300

    Add stable symmetry audit after failed normalization attempt

---
+++ FMT/Graph/DistanceSymmetryStableAudit.lean
@@ -0,0 +1,12 @@
+import FMT.Graph.DistanceSymmetry
+import FMT.Graph.PathLengthReverse
+import FMT.Graph.DistancePath
+
+namespace FMT.Graph
+
+#print dist?_symm
+#print pathLength_reverse
+#print path_of_dist?_some
+#print dist?_le_of_path
+
+end FMT.Graph
