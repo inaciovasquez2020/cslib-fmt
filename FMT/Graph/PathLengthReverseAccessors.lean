d422635298e198b6d7f9595dd62ded1c8d7e509b
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 07:06:09 -0300

    Audit reverse path interface against accessor layer

---
+++ FMT/Graph/PathLengthReverseAccessors.lean
@@ -0,0 +1,11 @@
+import FMT.Graph.PathLengthReverse
+import FMT.Graph.PathLengthAccessors
+
+namespace FMT.Graph
+
+#print pathLength_reverse
+#print PathLength.first_eq
+#print PathLength.last_eq
+#print PathLength.start_adj
+
+end FMT.Graph
