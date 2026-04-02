b46c0c9294322a07cdfd8d20ed8e884222240674
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 00:26:55 -0300

    Add PathLength core structure

---
+++ FMT/Graph/PathLength.lean
@@ -0,0 +1,14 @@
+import FMT.Graph.Basic
+
+namespace FMT.Graph
+
+structure PathLength (G : Graph) (u v : G.V) (n : Nat) : Prop where
+  verts : Fin (n + 1) → G.V
+  start : verts ⟨0, Nat.succ_pos _⟩ = u
+  finish : verts ⟨n, Nat.lt_succ_self n⟩ = v
+  step :
+    ∀ i : Fin n,
+      G.Adj (verts ⟨i.1, Nat.lt_trans i.2 (Nat.lt_succ_self n)⟩)
+            (verts ⟨i.1 + 1, Nat.succ_lt_succ i.2⟩)
+
+end FMT.Graph
