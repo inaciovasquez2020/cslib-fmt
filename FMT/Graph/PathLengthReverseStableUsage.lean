800b220e30b624d5dc1c1fe81b7d28777abd8352
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 07:13:14 -0300

    Add stable reverse-path usage audit after symmetry restoration

---
+++ FMT/Graph/PathLengthReverseStableUsage.lean
@@ -0,0 +1,13 @@
+import FMT.Graph.PathLengthReverse
+import FMT.Graph.DistanceSymmetry
+import FMT.Graph.PathLengthAccessors
+
+namespace FMT.Graph
+
+#print pathLength_reverse
+#print dist?_symm
+#print PathLength.first_eq
+#print PathLength.last_eq
+#print PathLength.start_adj
+
+end FMT.Graph
