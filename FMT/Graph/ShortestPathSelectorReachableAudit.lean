50ce5feda73e4f5efc778305b2f537396a97cfd1
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-02 07:16:38 -0300

    Audit selector against reachable bridge

---
+++ FMT/Graph/ShortestPathSelectorReachableAudit.lean
@@ -0,0 +1,12 @@
+import FMT.Graph.ShortestPathSelector
+import FMT.Graph.ExistsMinPathLengthBridge
+import FMT.Graph.PathLengthLemmas
+
+namespace FMT.Graph
+
+#print Reachable
+#print exists_min_pathLength_of_reachable
+#print shortest_path_selector
+#print shortest_path_selector_complete
+
+end FMT.Graph
