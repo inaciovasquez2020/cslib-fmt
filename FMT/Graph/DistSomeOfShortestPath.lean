1940dd0ff8cd69cd7357c0865f1b6f8904b8dad2
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-03 10:09:20 -0300

    Add shortest-path selector axiom for canonical distance value

---
+++ FMT/Graph/DistSomeOfShortestPath.lean
@@ -0,0 +1,11 @@
+import FMT.Graph.DistancePath
+
+namespace FMT.Graph
+
+axiom dist?_some_of_shortest_path
+  (G : Graph) {u v : G.V} {d : Nat}
+  (hdpath : Nonempty (PathLength G u v d))
+  (hdmin : ∀ m, m < d → ¬ Nonempty (PathLength G u v m)) :
+  dist? G u v = some d
+
+end FMT.Graph
