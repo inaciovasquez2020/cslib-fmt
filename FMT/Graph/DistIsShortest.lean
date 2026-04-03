309cd3fbf9ebcef6bed582bea72e962d525347e8
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-03 10:35:35 -0300

    Replace dist?_is_shortest axiom with theorem from minimality layer

--- FMT/Graph/DistIsShortest.lean
+++ FMT/Graph/DistIsShortest.lean
@@ -1,10 +1,12 @@
-import FMT.Graph.DistancePath
+import FMT.Graph.DistanceMinimality
 
 namespace FMT.Graph
 
-axiom dist?_is_shortest
+theorem dist?_is_shortest
   (G : Graph) {u v : G.V} {n : Nat} :
   dist? G u v = some n →
-  ∀ m, m < n → ¬ Nonempty (PathLength G u v m)
+  ∀ m, m < n → ¬ Nonempty (PathLength G u v m) := by
+  intro h
+  exact (dist?_some_iff_shortest G).mp h |>.2
 
 end FMT.Graph
