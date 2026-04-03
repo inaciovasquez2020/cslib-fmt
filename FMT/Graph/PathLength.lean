84d226ebb15ec8e43b0e977a5b6494467b5b8855
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-03 11:43:11 -0300

    Complete PathLength structure with instance projections

--- FMT/Graph/PathLength.lean
+++ FMT/Graph/PathLength.lean
@@ -3,12 +3,9 @@ import FMT.Graph.Basic
 namespace FMT.Graph
 
 structure PathLength (G : Graph) (u v : G.V) (n : Nat) : Type where
-  verts : Fin (n + 1) → G.V
-  start : verts ⟨0, Nat.succ_pos _⟩ = u
-  finish : verts ⟨n, Nat.lt_succ_self n⟩ = v
-  step :
-    ∀ i : Fin n,
-      G.Adj (verts ⟨i.1, Nat.lt_trans i.2 (Nat.lt_succ_self n)⟩)
-            (verts ⟨i.1 + 1, Nat.succ_lt_succ i.2⟩)
+verts : Fin (n + 1) → G.V
+start : verts ⟨0, Nat.succ_pos n⟩ = u
+finish : verts ⟨n, Nat.lt_succ_self n⟩ = v
+step : ∀ i : Fin n, G.Adj (verts i.castSucc) (verts i.succ)
 
 end FMT.Graph
