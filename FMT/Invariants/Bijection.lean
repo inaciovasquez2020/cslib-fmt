2594cbd0abdc61c9092ddb060c7a46833407344c
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-04-04 22:22:46 -0300

    Refine LocalType to inductive two-point type

--- FMT/Invariants/Bijection.lean
+++ FMT/Invariants/Bijection.lean
@@ -1,18 +1,15 @@
-import FMT.Types.LocalType
-import FMT.Invariants.Eval
 import FMT.Invariants.Injectivity
 import FMT.Invariants.Surjectivity
 
 namespace FMT.Invariants
 
+open FMT.Types
+
 theorem eval_bijective :
   (∀ t₁ t₂ : FMT.Types.LocalType,
       evalLocal t₁ = evalLocal t₂ → t₁ = t₂) ∧
   (∀ n : Nat, n = 0 ∨ n = 1 →
-      ∃ t : FMT.Types.LocalType, evalLocal t = n) :=
-by
-  constructor
-  · exact eval_injective
-  · exact eval_surjective
+      ∃ t : FMT.Types.LocalType, evalLocal t = n) := by
+  refine ⟨eval_injective, eval_surjective01⟩
 
 end FMT.Invariants
--- FMT/Invariants/Injectivity.lean
+++ FMT/Invariants/Injectivity.lean
@@ -1,12 +1,13 @@
 import FMT.Types.LocalType
-import FMT.Invariants.Eval
 
 namespace FMT.Invariants
+
+open FMT.Types
 
 theorem eval_injective :
   ∀ t₁ t₂ : FMT.Types.LocalType,
     evalLocal t₁ = evalLocal t₂ → t₁ = t₂ := by
   intro t₁ t₂ h
-  cases t₁ <;> cases t₂ <;> simp [evalLocal] at h <;> try contradiction <;> rfl
+  cases t₁ <;> cases t₂ <;> simp [evalLocal] at h ⊢
 
 end FMT.Invariants
